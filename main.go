ff60facad99dd5b71779d5958fb6d3870069330c
Richard Bates <rich.bates@protonmail.com>
2023-11-01 22:29:32 +0000

    Structs and basic functions for orderbook, order and limits

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/kr/pretty"
+)
 
+func main() {
+	buyOrder := newOrder(Bid, 5)
+	sellOrder := newOrder(Ask, 5)
+	limit := newLimit(250)
+	limit.addOrder(*buyOrder)
+	limit.addOrder(*sellOrder)
+
+	orderbook := newOrderBook()
+	orderbook.addOrder(250, *buyOrder)
+	orderbook.addOrder(200, *buyOrder)
+
+	pretty.Print(orderbook)
 }
---
+++ orderbook.go
@@ -0,0 +1,61 @@
+package main
+
+type Side string
+
+const (
+	Bid Side = "bid"
+	Ask Side = "ask"
+)
+
+type Order struct {
+	side Side
+	size uint64
+}
+
+func newOrder(side Side, size uint64) *Order {
+	return &Order{side, size}
+}
+
+type Limit struct {
+	price  uint64
+	orders []Order
+}
+
+func newLimit(price uint64) *Limit {
+	return &Limit{
+		price:  price,
+		orders: make([]Order, 0),
+	}
+}
+
+func (limit *Limit) addOrder(order Order) {
+	limit.orders = append(limit.orders, order)
+}
+
+type Orderbook struct {
+	asks map[uint64]Limit
+	bids map[uint64]Limit
+}
+
+func newOrderBook() *Orderbook {
+	return &Orderbook{
+		asks: make(map[uint64]Limit),
+		bids: make(map[uint64]Limit),
+	}
+}
+
+func (book *Orderbook) addOrder(price uint64, order Order) {
+	if order.side == Bid {
+		// check if a limit already exists at the order price
+		limit, ok := book.bids[price]
+		if ok {
+			// if one exists, add the order to that limit
+			limit.addOrder(order)
+		} else {
+			// if doesn't exist, create a new limit with the order
+			newLimit := newLimit(price)
+			newLimit.addOrder(order)
+			book.bids[price] = *newLimit
+		}
+	}
+}
