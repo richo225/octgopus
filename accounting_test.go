2836a6e63909a6d964779e7b4dccd63fbbfb07ee
Richard Bates <rich.bates@protonmail.com>
2023-11-01 01:01:37 +0000

    Adds tests for withdraw and send functions

--- accounting_test.go
+++ accounting_test.go
@@ -62,3 +62,91 @@ func TestDepositExistingAccount(t *testing.T) {
 		t.Errorf("balanceOf(alice) = %d, expected 150", balance)
 	}
 }
+
+func TestWithdrawAccountNotFound(t *testing.T) {
+	accounts := newAccounts()
+
+	err := accounts.withdraw("alice", 50)
+	if err == nil {
+		t.Errorf("withdraw(alice, 50) returned nil error, expected AccountNotFoundError")
+	} else if _, ok := err.(*AccountNotFoundError); !ok {
+		t.Errorf("withdraw(alice, 50) returned %v, expected AccountNotFoundError", err)
+	}
+}
+
+func TestWithdrawUnderFunded(t *testing.T) {
+	accounts := newAccounts()
+	accounts.deposit("alice", 10)
+
+	// Test withdraw with an empty account.
+	err := accounts.withdraw("alice", 50)
+	if err == nil {
+		t.Errorf("withdraw(alice, 50) returned nil error, expected UnderFunded error")
+	} else if _, ok := err.(*AccountUnderFundedError); !ok {
+		t.Errorf("withdraw(alice, 50) returned %v, expected UnderFundedError", err)
+	}
+}
+
+func TestWithdrawSufficientBalance(t *testing.T) {
+	accounts := newAccounts()
+	accounts.deposit("alice", 100)
+
+	err := accounts.withdraw("alice", 20)
+	if err != nil {
+		t.Errorf("withdraw(alice, 50) returned error: %v", err)
+	}
+	balance, err := accounts.balanceOf("alice")
+	if err != nil {
+		t.Errorf("balanceOf(alice) returned error: %v", err)
+	} else if balance != 80 {
+		t.Errorf("balanceOf(alice) = %d, expected 80", balance)
+	}
+}
+
+func TestSendWithSenderNotFound(t *testing.T) {
+	accounts := newAccounts()
+
+	err := accounts.send("alice", "bob", 50)
+	if err == nil {
+		t.Errorf("send(alice, bob, 50) returned nil error, expected AccountNotFoundError")
+	} else if _, ok := err.(*AccountNotFoundError); !ok {
+		t.Errorf("send(alice, bob, 50) returned %v, expected AccountNotFoundError", err)
+	}
+}
+
+func TestSendWithSenderUnderFunded(t *testing.T) {
+	accounts := newAccounts()
+	accounts.deposit("alice", 25)
+
+	err := accounts.send("alice", "bob", 50)
+	if err == nil {
+		t.Errorf("send(alice, bob, 50) returned nil error, expected AccountUnderFundedError")
+	} else if _, ok := err.(*AccountUnderFundedError); !ok {
+		t.Errorf("send(alice, bob, 50) returned %v, expected AccountUnderFundedError", err)
+	}
+}
+
+func TestSendWSuccess(t *testing.T) {
+	accounts := newAccounts()
+	accounts.deposit("alice", 100)
+	accounts.deposit("bob", 10)
+
+	err := accounts.send("alice", "bob", 30)
+	if err != nil {
+		t.Errorf("send(alice, bob, 50) returned error: %v", err)
+	}
+
+	balance, err := accounts.balanceOf("alice")
+	if err != nil {
+		t.Errorf("balanceOf(alice) returned error: %v", err)
+	} else if balance != 70 {
+		t.Errorf("balanceOf(alice) = %d, expected 70", balance)
+	}
+
+	balance, err = accounts.balanceOf("bob")
+	if err != nil {
+		t.Errorf("balanceOf(bob) returned error: %v", err)
+	} else if balance != 40 {
+		t.Errorf("balanceOf(bob) = %d, expected 40", balance)
+	}
+}
