5228c4e51640cd5a253aae70e132a35b25b50420
Richard Bates <rich.bates@protonmail.com>
2023-11-03 17:48:05 +0000

    Sorts orderbook limits by side

--- orderbook.go
+++ orderbook.go
@@ -1,21 +1,27 @@
 package main
 
+import "sort"
+
 type Orderbook struct {
-	asks map[uint64]*Limit
-	bids map[uint64]*Limit
+	asks      []*Limit
+	bids      []*Limit
+	askLimits map[uint64]*Limit
+	bidLimits map[uint64]*Limit
 }
 
 func newOrderBook() *Orderbook {
 	return &Orderbook{
-		asks: make(map[uint64]*Limit),
-		bids: make(map[uint64]*Limit),
+		asks:      []*Limit{},
+		bids:      []*Limit{},
+		askLimits: make(map[uint64]*Limit),
+		bidLimits: make(map[uint64]*Limit),
 	}
 }
 
-func (book *Orderbook) addOrder(price uint64, order *Order) {
+func (book *Orderbook) placeLimitOrder(price uint64, order *Order) {
 	if order.side == Bid {
 		// check if a limit already exists at the order price
-		limit, ok := book.bids[price]
+		limit, ok := book.bidLimits[price]
 		if ok {
 			// if one exists, add the order to that limit
 			limit.addOrder(order)
@@ -23,16 +29,34 @@ func (book *Orderbook) addOrder(price uint64, order *Order) {
 			// if doesn't exist, create a new limit with the order
 			newLimit := newLimit(price)
 			newLimit.addOrder(order)
-			book.bids[price] = newLimit
+			book.bidLimits[price] = newLimit
+			book.bids = append(book.bids, newLimit)
 		}
 	} else {
-		limit, ok := book.asks[price]
+		limit, ok := book.askLimits[price]
 		if ok {
 			limit.addOrder(order)
 		} else {
 			newLimit := newLimit(price)
 			newLimit.addOrder(order)
-			book.asks[price] = newLimit
+			book.askLimits[price] = newLimit
+			book.asks = append(book.asks, newLimit)
 		}
 	}
+}
+
+func (book *Orderbook) Asks() []*Limit {
+	sort.Slice(book.asks, func(i, j int) bool {
+		return book.asks[i].price < book.asks[j].price
+	})
+
+	return book.asks
+}
+
+func (book *Orderbook) Bids() []*Limit {
+	sort.Slice(book.bids, func(i, j int) bool {
+		return book.bids[i].price > book.bids[j].price
+	})
+
+	return book.bids
 }
--- orderbook_test.go
+++ orderbook_test.go
@@ -13,16 +13,45 @@ func TestNewOrderBook(t *testing.T) {
 	assert.Empty(t, orderBook.bids, "order book should initialise with no bids")
 }
 
-func TestOrderBookAddOrder(t *testing.T) {
+func TestOrderBookPlaceLimitOrder(t *testing.T) {
 	orderBook := newOrderBook()
 	buyOrder := newOrder(Bid, 5)
 	sellOrder := newOrder(Ask, 5)
 
-	orderBook.addOrder(250, buyOrder)
-	orderBook.addOrder(120, sellOrder)
+	orderBook.placeLimitOrder(250, buyOrder)
+	orderBook.placeLimitOrder(120, sellOrder)
 
 	assert.Equal(t, 1, len(orderBook.bids), "order book should have 1 limit in bids")
-	assert.Equal(t, buyOrder, orderBook.bids[250].orders[0], "order book should have the correct buy order in bids")
+	assert.Equal(t, buyOrder, orderBook.bidLimits[250].orders[0], "order book should have the correct buy order in bidLimits")
+	assert.Equal(t, buyOrder, orderBook.bids[0].orders[0], "order book should have the correct buy order in bids")
+
 	assert.Equal(t, 1, len(orderBook.asks), "order book should have 1 limit in asks")
-	assert.Equal(t, sellOrder, orderBook.asks[120].orders[0], "order book should have the correct sell order in asks")
+	assert.Equal(t, sellOrder, orderBook.askLimits[120].orders[0], "order book should have the correct sell order in askLimits")
+	assert.Equal(t, sellOrder, orderBook.asks[0].orders[0], "order book should have the correct buy order in bids")
+}
+
+func TestOrderBookAsks(t *testing.T) {
+	orderbook := newOrderBook()
+	limit1 := newLimit(12)
+	limit2 := newLimit(8)
+	limit3 := newLimit(25)
+
+	orderbook.asks = []*Limit{limit1, limit2, limit3}
+	sortedAsks := orderbook.Asks()
+
+	assert.Equal(t, 3, len(sortedAsks), "Asks() should return 3 limits")
+	assert.Equal(t, []*Limit{limit2, limit1, limit3}, sortedAsks, "Asks() should return limits in ascending price")
+}
+
+func TestOrderBookBids(t *testing.T) {
+	orderbook := newOrderBook()
+	limit1 := newLimit(12)
+	limit2 := newLimit(8)
+	limit3 := newLimit(25)
+
+	orderbook.bids = []*Limit{limit1, limit2, limit3}
+	sortedBids := orderbook.Bids()
+
+	assert.Equal(t, 3, len(sortedBids), "Bids() should return 3 limits")
+	assert.Equal(t, []*Limit{limit3, limit1, limit2}, sortedBids, "Bids() should return limits in descending price")
 }
