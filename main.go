2068c27a989a720fb083a278ca63b93d2ad433fd
Richard Bates <rich.bates@protonmail.com>
2023-11-04 20:14:12 +0000

    clear main

--- main.go
+++ main.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"github.com/kr/pretty"
-)
-
 func main() {
-	buyOrder := newOrder(Bid, 5)
-	sellOrder := newOrder(Ask, 5)
-	limit := newLimit(250)
-	limit.addOrder(buyOrder)
-	limit.addOrder(sellOrder)
-
-	orderbook := newOrderBook()
-	orderbook.addOrder(250, buyOrder)
-	orderbook.addOrder(200, sellOrder)
-
-	pair := newTradingPair("ETH", "GBP")
-	engine := newMatchingEngine()
-	engine.addNewMarket(pair)
-	engine.orderbooks[pair] = *orderbook
-
-	pretty.Print(engine)
 }
