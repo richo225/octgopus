6355101a008b88001e8fb80a79d358b2fa6a67e0
Richard Bates <rich.bates@protonmail.com>
2023-11-04 00:03:31 +0000

    Adds matching and filling of limit orders

--- limit.go
+++ limit.go
@@ -1,5 +1,12 @@
 package main
 
+type Match struct {
+	ask        *Order
+	bid        *Order
+	sizeFilled uint64
+	price      uint64
+}
+
 type Limit struct {
 	price       uint64
 	orders      []*Order
@@ -19,26 +26,56 @@ func (limit *Limit) addOrder(order *Order) {
 	limit.totalVolume += order.size
 }
 
-func (limit *Limit) matchOrder(order *Order) {
+func (limit *Limit) matchOrder(order *Order) []Match {
+	matches := []Match{}
+
 	// iterate through each order in the limit
 	for _, limitOrder := range limit.orders {
 		// fill the order
-		limit.fillOrders(limitOrder, order)
+		match := limit.fillOrders(limitOrder, order)
+		// add the match to the list of matches
+		matches = append(matches, match)
 
+		// if the order is filled, break
 		if order.size == 0 {
 			break
 		}
 	}
+
+	return matches
 }
 
-func (limit *Limit) fillOrders(limitOrder, order *Order) {
+func (limit *Limit) fillOrders(limitOrder, order *Order) Match {
+	var (
+		ask        *Order
+		bid        *Order
+		sizeFilled uint64
+	)
+
+	if order.side == Bid {
+		bid = order
+		ask = limitOrder
+	} else {
+		bid = limitOrder
+		ask = order
+	}
+
 	// if the order is smaller than the limit order, fill the order
 	if limitOrder.size >= order.size {
 		limitOrder.size -= order.size
+		sizeFilled = order.size
 		order.size = 0
 	} else {
 		// if the order is larger than the limit order, fill the limit order
 		order.size -= limitOrder.size
+		sizeFilled = limitOrder.size
 		limitOrder.size = 0
+	}
+
+	return Match{
+		ask:        ask,
+		bid:        bid,
+		sizeFilled: sizeFilled,
+		price:      limit.price,
 	}
 }
--- limit_test.go
+++ limit_test.go
@@ -23,3 +23,34 @@ func TestLimitAddOrder(t *testing.T) {
 	assert.Equal(t, order, limit.orders[0], "limit order should be new order")
 	assert.Equal(t, uint64(5), limit.totalVolume, "limit total volume should be 5")
 }
+
+func TestLimitMatchOrder(t *testing.T) {
+	limit := newLimit(250)
+	sellOrder := newOrder(Ask, 5)
+	buyOrder := newOrder(Bid, 3)
+
+	limit.addOrder(sellOrder)
+
+	matches := limit.matchOrder(buyOrder)
+
+	assert.Equal(t, 1, len(matches), "limit should have 1 match")
+	assert.Equal(t, buyOrder, matches[0].bid, "match bid should be order")
+	assert.Equal(t, sellOrder, matches[0].ask, "match ask should be order")
+	assert.Equal(t, uint64(3), matches[0].sizeFilled, "match size filled should be 3")
+	assert.Equal(t, uint64(250), matches[0].price, "match price should be 250")
+}
+
+func TestLimitFillOrders(t *testing.T) {
+	limit := newLimit(250)
+	sellOrder := newOrder(Ask, 1)
+	buyOrder := newOrder(Bid, 3)
+
+	limit.addOrder(buyOrder)
+
+	match := limit.fillOrders(buyOrder, sellOrder)
+
+	assert.Equal(t, buyOrder, match.bid, "match bid should be order")
+	assert.Equal(t, sellOrder, match.ask, "match ask should be order")
+	assert.Equal(t, uint64(1), match.sizeFilled, "match size filled should be 1")
+	assert.Equal(t, uint64(250), match.price, "match price should be 250")
+}
