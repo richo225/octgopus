adc40abbc02bbb4c223672d457c8cc597e1d2c35
Richard Bates <rich.bates@protonmail.com>
2023-11-04 00:03:41 +0000

    Place market order logic

--- orderbook.go
+++ orderbook.go
@@ -1,6 +1,18 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
+
+type InsufficientVolumeError struct {
+	available uint64
+	requested uint64
+}
+
+func (e *InsufficientVolumeError) Error() string {
+	return "InsufficientVolume : " + fmt.Sprint(e.available) + " < " + fmt.Sprint(e.requested)
+}
 
 type Orderbook struct {
 	asks      []*Limit
@@ -16,6 +28,56 @@ func newOrderBook() *Orderbook {
 		askLimits: make(map[uint64]*Limit),
 		bidLimits: make(map[uint64]*Limit),
 	}
+}
+
+func (book *Orderbook) Asks() []*Limit {
+	sort.Slice(book.asks, func(i, j int) bool {
+		return book.asks[i].price < book.asks[j].price
+	})
+
+	return book.asks
+}
+
+func (book *Orderbook) Bids() []*Limit {
+	sort.Slice(book.bids, func(i, j int) bool {
+		return book.bids[i].price > book.bids[j].price
+	})
+
+	return book.bids
+}
+
+func (book *Orderbook) bestAsk() *Limit {
+	if len(book.asks) == 0 {
+		return nil
+	}
+
+	return book.Asks()[0]
+}
+
+func (book *Orderbook) bestBid() *Limit {
+	if len(book.bids) == 0 {
+		return nil
+	}
+
+	return book.Bids()[0]
+}
+
+func (book *Orderbook) totalBidVolume() uint64 {
+	var total uint64
+	for _, limit := range book.bids {
+		total += limit.totalVolume
+	}
+
+	return total
+}
+
+func (book *Orderbook) totalAskVolume() uint64 {
+	var total uint64
+	for _, limit := range book.asks {
+		total += limit.totalVolume
+	}
+
+	return total
 }
 
 func (book *Orderbook) placeLimitOrder(price uint64, order *Order) {
@@ -45,14 +107,22 @@ func (book *Orderbook) placeLimitOrder(price uint64, order *Order) {
 	}
 }
 
-func (book *Orderbook) placeMarketOrder(order *Order) {
+func (book *Orderbook) placeMarketOrder(order *Order) ([]Match, error) {
+	matches := []Match{}
+
 	// check which side order is
 	if order.side == Bid {
+		// if total ask volume is less than order size, return error
+		if book.totalAskVolume() < order.size {
+			return nil, &InsufficientVolumeError{book.totalAskVolume(), order.size}
+		}
+
 		// get all the sorted asks/bids of opposite side
 		// iterate through each limit (market order so start with smallest price)
 		for _, limit := range book.Asks() {
 			// attempt to match the order to the limit
-			limit.matchOrder(order)
+			limitMatches := limit.matchOrder(order)
+			matches = append(matches, limitMatches...)
 			// if the order is filled, break
 			if order.size == 0 {
 				break
@@ -60,27 +130,19 @@ func (book *Orderbook) placeMarketOrder(order *Order) {
 
 		}
 	} else {
+		// if total bid volume is less than order size, return error
+		if book.totalBidVolume() < order.size {
+			return nil, &InsufficientVolumeError{book.totalBidVolume(), order.size}
+		}
+
 		for _, limit := range book.Bids() {
-			limit.matchOrder(order)
+			limitMatches := limit.matchOrder(order)
+			matches = append(matches, limitMatches...)
 			if order.size == 0 {
 				break
 			}
 		}
 	}
-}
 
-func (book *Orderbook) Asks() []*Limit {
-	sort.Slice(book.asks, func(i, j int) bool {
-		return book.asks[i].price < book.asks[j].price
-	})
-
-	return book.asks
-}
-
-func (book *Orderbook) Bids() []*Limit {
-	sort.Slice(book.bids, func(i, j int) bool {
-		return book.bids[i].price > book.bids[j].price
-	})
-
-	return book.bids
+	return matches, nil
 }
--- orderbook_test.go
+++ orderbook_test.go
@@ -13,23 +13,6 @@ func TestNewOrderBook(t *testing.T) {
 	assert.Empty(t, orderBook.bids, "order book should initialise with no bids")
 }
 
-func TestOrderBookPlaceLimitOrder(t *testing.T) {
-	orderBook := newOrderBook()
-	buyOrder := newOrder(Bid, 5)
-	sellOrder := newOrder(Ask, 5)
-
-	orderBook.placeLimitOrder(250, buyOrder)
-	orderBook.placeLimitOrder(120, sellOrder)
-
-	assert.Equal(t, 1, len(orderBook.bids), "order book should have 1 limit in bids")
-	assert.Equal(t, buyOrder, orderBook.bidLimits[250].orders[0], "order book should have the correct buy order in bidLimits")
-	assert.Equal(t, buyOrder, orderBook.bids[0].orders[0], "order book should have the correct buy order in bids")
-
-	assert.Equal(t, 1, len(orderBook.asks), "order book should have 1 limit in asks")
-	assert.Equal(t, sellOrder, orderBook.askLimits[120].orders[0], "order book should have the correct sell order in askLimits")
-	assert.Equal(t, sellOrder, orderBook.asks[0].orders[0], "order book should have the correct buy order in bids")
-}
-
 func TestOrderBookAsks(t *testing.T) {
 	orderbook := newOrderBook()
 	limit1 := newLimit(12)
@@ -54,4 +37,189 @@ func TestOrderBookBids(t *testing.T) {
 
 	assert.Equal(t, 3, len(sortedBids), "Bids() should return 3 limits")
 	assert.Equal(t, []*Limit{limit3, limit1, limit2}, sortedBids, "Bids() should return limits in descending price")
+}
+
+func TestOrderBookPlaceLimitOrder(t *testing.T) {
+	orderbook := newOrderBook()
+	buyOrder := newOrder(Bid, 5)
+	sellOrder := newOrder(Ask, 5)
+
+	orderbook.placeLimitOrder(250, buyOrder)
+	orderbook.placeLimitOrder(120, sellOrder)
+
+	assert.Equal(t, 1, len(orderbook.bids), "order book should have 1 limit in bids")
+	assert.Equal(t, buyOrder, orderbook.bidLimits[250].orders[0], "order book should have the correct buy order in bidLimits")
+	assert.Equal(t, buyOrder, orderbook.bids[0].orders[0], "order book should have the correct buy order in bids")
+
+	assert.Equal(t, 1, len(orderbook.asks), "order book should have 1 limit in asks")
+	assert.Equal(t, sellOrder, orderbook.askLimits[120].orders[0], "order book should have the correct sell order in askLimits")
+	assert.Equal(t, sellOrder, orderbook.asks[0].orders[0], "order book should have the correct buy order in bids")
+}
+
+func TestOrderBookPlaceMarketBuyOrder(t *testing.T) {
+	orderbook := newOrderBook()
+	buyOrder := newOrder(Bid, 3)
+	sellOrder := newOrder(Ask, 8)
+
+	orderbook.placeLimitOrder(250, sellOrder)
+	expectedMatches := []Match{{
+		sellOrder,
+		buyOrder,
+		3,
+		250,
+	}}
+	actualMatches, _ := orderbook.placeMarketOrder(buyOrder)
+	assert.Equal(t, expectedMatches, actualMatches, "placeMarketOrder should return correct matches")
+
+	// buyOrder size should be 0
+	assert.Equal(t, uint64(0), buyOrder.size, "buy order size should be 0")
+	// sellOrder size should be 5
+	assert.Equal(t, uint64(5), sellOrder.size, "sell order size should be 5")
+}
+
+func TestOrderBookPlaceMarketBuyOrderMultiMatch(t *testing.T) {
+	orderbook := newOrderBook()
+	buyOrder := newOrder(Bid, 3)
+	sellOrder1 := newOrder(Ask, 8)
+	sellOrder2 := newOrder(Ask, 2)
+
+	orderbook.placeLimitOrder(250, sellOrder1)
+	orderbook.placeLimitOrder(250, sellOrder2)
+	expectedMatches := []Match{{
+		sellOrder1,
+		buyOrder,
+		3,
+		250,
+	}}
+	actualMatches, _ := orderbook.placeMarketOrder(buyOrder)
+	assert.Equal(t, expectedMatches, actualMatches, "placeMarketOrder should return correct matches")
+
+	assert.Equal(t, uint64(0), buyOrder.size, "buy order size should be 0")
+	assert.Equal(t, uint64(5), sellOrder1.size, "sell order size should be 5")
+	assert.Equal(t, uint64(2), sellOrder2.size, "sell order size should be 2")
+}
+
+func TestOrderBookPlaceMarketBuyOrderMultiPriceLimitMatch(t *testing.T) {
+	orderbook := newOrderBook()
+	buyOrder := newOrder(Bid, 3)
+	sellOrder1 := newOrder(Ask, 8)
+	sellOrder2 := newOrder(Ask, 2)
+
+	orderbook.placeLimitOrder(250, sellOrder1)
+	orderbook.placeLimitOrder(240, sellOrder2)
+	expectedMatches := []Match{
+		{
+			sellOrder2,
+			buyOrder,
+			2,
+			240,
+		},
+		{
+			sellOrder1,
+			buyOrder,
+			1,
+			250,
+		}}
+	actualMatches, _ := orderbook.placeMarketOrder(buyOrder)
+	assert.Equal(t, expectedMatches, actualMatches, "placeMarketOrder should return correct matches")
+
+	assert.Equal(t, uint64(0), buyOrder.size, "buy order size should be 0")
+	assert.Equal(t, uint64(7), sellOrder1.size, "sell order size should be 7")
+	assert.Equal(t, uint64(0), sellOrder2.size, "sell order size should be 8")
+}
+
+func TestOrderBookPlaceMarketBuyOrderInsufficientVolume(t *testing.T) {
+	orderbook := newOrderBook()
+	buyOrder := newOrder(Bid, 3)
+	sellOrder := newOrder(Ask, 2)
+
+	orderbook.placeLimitOrder(250, sellOrder)
+
+	_, err := orderbook.placeMarketOrder(buyOrder)
+	assert.Equal(t, &InsufficientVolumeError{2, 3}, err, "placeMarketOrder should return InsufficientVolumeError")
+	assert.Equal(t, uint64(3), buyOrder.size, "buy order size should be 3")
+}
+
+// ffffffffff
+
+func TestOrderBookPlaceMarketSellOrder(t *testing.T) {
+	orderbook := newOrderBook()
+	sellOrder := newOrder(Ask, 3)
+	buyOrder := newOrder(Bid, 8)
+
+	orderbook.placeLimitOrder(250, buyOrder)
+	expectedMatches := []Match{{
+		sellOrder,
+		buyOrder,
+		3,
+		250,
+	}}
+	actualMatches, _ := orderbook.placeMarketOrder(sellOrder)
+	assert.Equal(t, expectedMatches, actualMatches, "placeMarketOrder should return correct matches")
+
+	assert.Equal(t, uint64(5), buyOrder.size, "buy order size should be 5")
+	assert.Equal(t, uint64(0), sellOrder.size, "sell order size should be 0")
+}
+
+func TestOrderBookPlaceMarketSellOrderMultiMatch(t *testing.T) {
+	orderbook := newOrderBook()
+	sellOrder := newOrder(Ask, 3)
+	buyOrder1 := newOrder(Bid, 8)
+	buyOrder2 := newOrder(Bid, 2)
+
+	orderbook.placeLimitOrder(250, buyOrder1)
+	orderbook.placeLimitOrder(250, buyOrder2)
+	expectedMatches := []Match{{
+		sellOrder,
+		buyOrder1,
+		3,
+		250,
+	}}
+	actualMatches, _ := orderbook.placeMarketOrder(sellOrder)
+	assert.Equal(t, expectedMatches, actualMatches, "placeMarketOrder should return correct matches")
+
+	assert.Equal(t, uint64(0), sellOrder.size, "sell order size should be 0")
+	assert.Equal(t, uint64(5), buyOrder1.size, "buy order size should be 5")
+	assert.Equal(t, uint64(2), buyOrder2.size, "sell order size should be 2")
+}
+
+func TestOrderBookPlaceMarketSellOrderMultiPriceLimitMatch(t *testing.T) {
+	orderbook := newOrderBook()
+	sellOrder := newOrder(Ask, 3)
+	buyOrder1 := newOrder(Bid, 8)
+	buyOrder2 := newOrder(Bid, 2)
+
+	orderbook.placeLimitOrder(250, buyOrder2)
+	orderbook.placeLimitOrder(240, buyOrder1)
+	expectedMatches := []Match{
+		{
+			sellOrder,
+			buyOrder2,
+			2,
+			250,
+		},
+		{
+			sellOrder,
+			buyOrder1,
+			1,
+			240,
+		}}
+	actualMatches, _ := orderbook.placeMarketOrder(sellOrder)
+	assert.Equal(t, expectedMatches, actualMatches, "placeMarketOrder should return correct matches")
+
+	assert.Equal(t, uint64(0), sellOrder.size, "sell order size should be 0")
+	assert.Equal(t, uint64(7), buyOrder1.size, "buy order size should be 7")
+	assert.Equal(t, uint64(0), buyOrder2.size, "buy order size should be 0")
+}
+
+func TestOrderBookPlaceMarketSellOrderInsufficientVolume(t *testing.T) {
+	orderbook := newOrderBook()
+	sellOrder := newOrder(Ask, 3)
+	buyOrder := newOrder(Bid, 2)
+
+	orderbook.placeLimitOrder(250, buyOrder)
+
+	_, err := orderbook.placeMarketOrder(sellOrder)
+	assert.Equal(t, &InsufficientVolumeError{2, 3}, err, "placeMarketOrder should return InsufficientVolumeError")
+	assert.Equal(t, uint64(3), sellOrder.size, "sell order size should be 3")
 }
