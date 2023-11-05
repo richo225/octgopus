10e27a4f7165e5a29612f43cb3c06fd42a7cdf51
Richard Bates <rich.bates@protonmail.com>
2023-11-05 21:08:31 +0000

    Update acounting handlers to use echo routing

--- handlers.go
+++ handlers.go
@@ -3,51 +3,101 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
-	"github.com/kr/pretty"
+	"github.com/labstack/echo/v4"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	pretty.Print("Hello")
+func sayHello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
 
 // Orderbooks
-func (platform *TradingPlatform) handleGetOrderbooks(w http.ResponseWriter, r *http.Request) {
-
+func (platform *TradingPlatform) handleGetOrderbooks(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+	return c.JSON(http.StatusOK, "Orderbooks")
 }
 
 // Orders
-func (platform *TradingPlatform) handleCreateOrder(w http.ResponseWriter, r *http.Request) {
-
+func (platform *TradingPlatform) handleCreateOrder(c echo.Context) error {
+	return c.String(http.StatusOK, "Order")
 }
 
 // Accounting
-func (platform *TradingPlatform) handleCreateAccount(w http.ResponseWriter, r *http.Request) {
-	signer := r.Context().Value("signer").(string)
+func (platform *TradingPlatform) handleCreateAccount(c echo.Context) error {
+	signer := c.Param("signer")
+
 	err := platform.accounts.createAccount(signer)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusConflict)
-		return
+		return err
 	}
 
-	w.Write([]byte(fmt.Sprintf("Account created for %s", signer)))
+	return c.String(http.StatusOK, "Account created")
 }
 
-func (platform *TradingPlatform) handleGetAccountBalance(w http.ResponseWriter, r *http.Request) {
-	signer := r.Context().Value("signer").(string)
+func (platform *TradingPlatform) handleGetAccountBalance(c echo.Context) error {
+	signer := c.Param("signer")
 	balance, err := platform.accounts.balanceOf(signer)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
+		return echo.NewHTTPError(http.StatusNotFound, err.Error())
 	}
 
-	w.Write([]byte(fmt.Sprintf("Balance: %d", balance)))
+	return c.String(http.StatusOK, fmt.Sprintf("%d", balance))
 }
 
-func (platform *TradingPlatform) handleAccountDeposit(w http.ResponseWriter, r *http.Request) {
+func (platform *TradingPlatform) handleAccountDeposit(c echo.Context) error {
+	signer := c.Param("signer")
+	amountStr := c.QueryParam("amount")
+	amount, err := strconv.ParseUint(amountStr, 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
 
+	tx, err := platform.accounts.deposit(signer, amount)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, &tx)
 }
 
-func (platform *TradingPlatform) handleAccountWithdraw(w http.ResponseWriter, r *http.Request) {
+func (platform *TradingPlatform) handleAccountWithdraw(c echo.Context) error {
+	signer := c.Param("signer")
+	amountStr := c.QueryParam("amount")
+	amount, err := strconv.ParseUint(amountStr, 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
 
+	tx, err := platform.accounts.withdraw(signer, amount)
+	if err != nil {
+		if _, ok := err.(*AccountNotFoundError); ok {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		} else if _, ok := err.(*AccountUnderFundedError); ok {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+	}
+
+	return c.JSON(http.StatusOK, &tx)
+}
+
+func (platform *TradingPlatform) handleAccountSend(c echo.Context) error {
+	signer := c.Param("signer")
+	recipient := c.QueryParam("recipient")
+	amountStr := c.QueryParam("amount")
+	amount, err := strconv.ParseUint(amountStr, 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	tx, err := platform.accounts.send(signer, recipient, amount)
+	if err != nil {
+		if _, ok := err.(*AccountNotFoundError); ok {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		} else if _, ok := err.(*AccountUnderFundedError); ok {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+	}
+
+	return c.JSON(http.StatusOK, &tx)
 }
--- main.go
+++ main.go
@@ -1,57 +1,30 @@
 package main
 
 import (
-	"context"
-	"net/http"
-	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kr/pretty"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
+	e := echo.New()
 	p := newTradingPlatform()
 
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.Timeout(30 * time.Second))
+	e.Use(middleware.RequestID())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	r.Get("/", sayHello)
+	e.GET("/", sayHello)
 
-	// GET /orderbooks?base=ETH&qoute=USD
-	r.Route("/orderbooks", func(chi.Router) {
-		r.Get("/", p.handleGetOrderbooks)
-	})
-	// POST /orders
-	r.Route("/orders", func(r chi.Router) {
-		r.Post("/", p.handleCreateOrder)
-	})
+	e.GET("/orderbooks", p.handleGetOrderbooks)
+	e.POST("/orders", p.handleCreateOrder)
 
-	// GET accounts/:signer/balance
-	// POST accounts/:signer/withdraw
-	// POST accounts/:signer/deposit
-	r.Route("/accounts", func(r chi.Router) {
-		r.Route("/{signer}", func(r chi.Router) {
-			r.Use(AccountCtx)
-			r.Get("/", p.handleGetAccountBalance)
-			r.Post("/", p.handleCreateAccount)
-			r.Post("/{signer}/deposit", p.handleAccountDeposit)
-			r.Post("/{signer}/withdraw", p.handleAccountWithdraw)
-		})
-	})
+	e.GET("/accounts/:signer", p.handleGetAccountBalance)
+	e.POST("/accounts/:signer", p.handleCreateAccount)
+	e.POST("/accounts/:signer/deposit", p.handleAccountDeposit)
+	e.POST("/accounts/:signer/withdraw", p.handleAccountWithdraw)
+	e.POST("/accounts/:signer/send", p.handleAccountSend)
 
 	pretty.Log("Starting server...")
-	http.ListenAndServe(":8080", r)
-}
-
-func AccountCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		signer := chi.URLParam(r, "signer")
-		ctx := context.WithValue(r.Context(), "signer", signer)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
