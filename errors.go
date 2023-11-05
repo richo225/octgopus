0765d25ac90a4a3cbc837bf46487dfb474f8ccd4
Richard Bates <rich.bates@protonmail.com>
2023-11-05 22:12:27 +0000

    Move errors into own file

---
+++ errors.go
@@ -0,0 +1,25 @@
+package main
+
+type AccountNotFoundError struct {
+	signer string
+}
+
+func (e *AccountNotFoundError) Error() string {
+	return "AccountNotFound: " + e.signer
+}
+
+type AccountAlreadyExistsError struct {
+	signer string
+}
+
+func (e *AccountAlreadyExistsError) Error() string {
+	return "AccountAlreadyExists: " + e.signer
+}
+
+type AccountUnderFundedError struct {
+	signer string
+}
+
+func (e *AccountUnderFundedError) Error() string {
+	return "AccountUnderFunded: " + e.signer
+}
