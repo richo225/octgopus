15f2c465b55669fae802713b87f8da8031630579
Richard Bates <rich.bates@protonmail.com>
2023-11-09 22:03:51 +0000

    WIP seeding orderbooks from file

--- seeds.go
+++ seeds.go
@@ -1,31 +1,61 @@
 package main
 
-func (platform *TradingPlatform) seedData() {
-	platform.accounts.deposit("Alice", 25000)
-	platform.accounts.deposit("Bob", 460)
-	platform.accounts.deposit("Charlie", 2690)
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+)
 
+func (platform *TradingPlatform) seedData() {
 	ethusd := newTradingPair("ETH", "USD")
-	ethgp := newTradingPair("ETH", "GBP")
+	ethgbp := newTradingPair("ETH", "GBP")
 	btcusd := newTradingPair("BTC", "USD")
 	btcgbp := newTradingPair("BTC", "GBP")
 
-	platform.addNewMarket(ethusd)
-	platform.addNewMarket(ethgp)
-	platform.addNewMarket(btcusd)
-	platform.addNewMarket(btcgbp)
+	platform.writeFromFile("data/eth_usd_order_book.csv", ethusd)
+	platform.writeFromFile("data/eth_gbp_order_book.csv", ethgbp)
+	platform.writeFromFile("data/btc_usd_order_book.csv", btcusd)
+	platform.writeFromFile("data/btc_gbp_order_book.csv", btcgbp)
+}
 
-	platform.placeLimitOrder(ethusd, 1680, newOrder(Bid, 2))
-	platform.placeLimitOrder(ethusd, 1640, newOrder(Bid, 5))
-	platform.placeLimitOrder(ethusd, 1620, newOrder(Bid, 5))
-	platform.placeLimitOrder(ethusd, 1700, newOrder(Ask, 1))
-	platform.placeLimitOrder(ethusd, 1720, newOrder(Ask, 4))
-	platform.placeLimitOrder(ethusd, 1740, newOrder(Ask, 12))
+func (platform *TradingPlatform) writeFromFile(filepath string, pair TradingPair) {
+	// Open the CSV file
+	f, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-	platform.placeLimitOrder(btcusd, 1680, newOrder(Bid, 2))
-	platform.placeLimitOrder(btcusd, 1640, newOrder(Bid, 5))
-	platform.placeLimitOrder(btcusd, 1620, newOrder(Bid, 5))
-	platform.placeLimitOrder(btcusd, 1700, newOrder(Ask, 1))
-	platform.placeLimitOrder(btcusd, 1720, newOrder(Ask, 4))
-	platform.placeLimitOrder(btcusd, 1740, newOrder(Ask, 12))
+	// Create a new reader
+	r := csv.NewReader(f)
+	// Read the header row
+	_, err = r.Read()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create market for pair
+	platform.addNewMarket(pair)
+
+	// Iterate over the remaining rows
+	for {
+		record, err := r.Read()
+		if err != nil {
+			break
+		}
+
+		// Parse price and amount
+		askPrice, _ := strconv.ParseUint(record[0], 10, 64)
+		askAmount, _ := strconv.ParseUint(record[1], 10, 64)
+		bidPrice, _ := strconv.ParseUint(record[2], 10, 64)
+		bidAmount, _ := strconv.ParseUint(record[3], 10, 64)
+
+		// Create and place ask order
+		askOrder := newOrder(Ask, askAmount)
+		platform.placeLimitOrder(pair, askPrice, askOrder)
+
+		// Create and place bid order
+		bidOrder := newOrder(Bid, bidAmount)
+		platform.placeLimitOrder(pair, bidPrice, bidOrder)
+	}
 }
